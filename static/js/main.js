f0af1f8cdad5cbc80e2ce3ee9d1c4f8fb32e4b4d
Paco <francisco@ancegri.mx>
2021-04-20 15:57:42 -0500

    1 - 20/abril/2021

--- static/js/main.js
+++ static/js/main.js
@@ -1,74 +1,14 @@
-let homePageList = document.getElementsByClassName('pageblock__page--home');
-let modeloPageList = document.getElementsByClassName('pageblock__page--modelo');
-let homeIndex = 0;
-let homePages = new Map();
-for(var i = 0; i < homePageList.length; i++){
-    homePages.set(i, homePageList[i]);
-}
-let homePagesIndexList = homePages.size;
-let modeloIndex = 0;
-let modeloPages = new Map();
-for(var i = 0; i < modeloPageList.length; i++){
-    modeloPages.set(i, modeloPageList[i]);
-}
-let modeloPagesIndexList = modeloPages.size;
-function nextHomeSlide(){
-    if(homeIndex == 0){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: -100vw;';
-        }
-        homeIndex++;
+let headerTitle = document.getElementById('title__principal');
+let headerNav = document.getElementById('navigation__principal');
+let navLinks = document.getElementsByClassName('navigation__link');
+let headerContainer = document.querySelector('header > div');
+window.addEventListener('scroll', function(){
+    headerTitle.classList.toggle('header__title--scroll', scrollY > 0);
+    headerNav.classList.toggle('header__navigation--scroll', scrollY > 0);
+    if(scrollY > 0){
+        headerContainer.style = 'background: var(--primary-color);height:10rem;transition:all 0.5s;';
     }
-    else if(homeIndex == 1){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: -200vw;';
-        }
-        homeIndex++;
+    else if(scrollY==0){
+        headerContainer.style = 'background: transparent;'
     }
-    else if(homeIndex == 2){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: -300vw;';
-        }
-        homeIndex++;
-    }
-    else{}
-}
-function prevHomeSlide(){
-    if(homeIndex == 0){}
-    else if(homeIndex == 1){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: 0vw;';
-        }
-        homeIndex--;
-    }
-    else if(homeIndex == 2){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: -100vw;';
-        }
-        homeIndex--;
-    }
-    else if(homeIndex == 3){
-        for (var i = 0; i < homePagesIndexList; i++){
-            homePages.get(i).style = 'left: -200vw;';
-        }
-        homeIndex--;
-    }
-}
-function nextModeloSlide(){
-    if(modeloIndex == 0){
-        for (var i = 0; i < modeloPagesIndexList; i++){
-            modeloPages.get(i).style = 'left: -100vw;';
-        }
-        modeloIndex++;
-    }
-    else{}
-}
-function prevModeloSlide(){
-    if(modeloIndex == 0){}
-    else if(modeloIndex == 1){
-        for (var i = 0; i < modeloPagesIndexList; i++){
-            modeloPages.get(i).style = 'left: 0vw;';
-        }
-        modeloIndex--;
-    }
-}
+});
